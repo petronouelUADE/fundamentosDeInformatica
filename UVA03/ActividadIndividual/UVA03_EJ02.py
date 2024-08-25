4cb9bdd82b4dd41521c797417c26599bff284976
Petro Nouel <petronouel@gmail.com>
2024-08-25 16:32:14 -0300

    ejercicio 2 y pdf tp

---
+++ UVA03/ActividadIndividual/UVA03_EJ02.py
@@ -0,0 +1,23 @@
+
+print(12 * 4 + 4 * 5) 
+#Orden: Multiplicaciones, suma
+print(12 * (4 + 4) * 5)
+#Orden: Suma parentesis, multiplcacion, multiplicacion --
+print(5 * 4 / 2)
+#Orden: Multiplicacion, division
+print(5 * (4 / 2))
+#Orden: Division, multiplicacion
+print(24 / 2 ** 2)
+#Orden: Potencia, division
+print(-9 ** 2)
+#Orden: Potencia, asignacion signo --
+print((-9) ** 2)
+#Orden: Parentesis(signo), potencia
+print(10 / 3)
+#Orden:division
+print(10 // 3)
+#Orden: Division entera
+print(12 % 5)
+#Orden: Resto
+
+ 
