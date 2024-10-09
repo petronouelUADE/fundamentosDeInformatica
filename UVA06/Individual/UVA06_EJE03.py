f7d2682371c85e54f0bdfbce94eff318e117b214
pnouel <petronouel@gmail.com>
2024-10-08 21:12:42 -0300

    Ejercicio 3

---
+++ UVA06/Individual/UVA06_EJE03.py
@@ -0,0 +1,16 @@
+"""
+Diseñar una función para mostrar un título filas de asteriscos, 
+la longitud de la fila de asteriscos y el texto del título se recibe como parámetro.
+Ejemplo: titulo(“Ejercicio 3”, 15) muestra:
+*************** 
+
+Ejercicio 3 
+
+*************** 
+"""
+
+def title(text, asterixLenght):
+    asterixs = "*" * asterixLenght
+    return print(f"{asterixs}\n\n  {text}\n\n{asterixs}")
+
+title("Ejercicio 3", 15)
